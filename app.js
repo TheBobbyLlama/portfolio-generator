afaf1b05519b969c02d64a5f3eb365853a5cf710
Bob Feuerstein <thebobbyllama@yahoo.com>
2020-07-26 16:00:51 -0500

    Finished site generation using Promises.

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
-
-//const profileDataArgs = process.argv.slice(2, process.argv.length);
-//const [name, github] = profileDataArgs;
-
-//  fs.writeFile('index.html', generatePage(name, github), err => {
-//	if (err) throw err;
-  
-//	console.log('Portfolio complete! Check out index.html to see the output!');
-//  });
 
 const promptUser = () => {
 	return inquirer.prompt([
@@ -135,15 +126,21 @@ const promptUser = () => {
 	  });
   };
   
-promptUser()
-	.then(promptProject)
-	.then(portfolioData => {
-    	const pageHTML = generatePage(portfolioData);
-
-    	fs.writeFile('./index.html', pageHTML, err => {
-			if (err) throw new Error(err);
-
-    	//   console.log('Page created! Check out index.html in this directory to see it!');
-    	});
-	}
-);
+  promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
